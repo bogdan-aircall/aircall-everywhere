e984a90c8bdea5b7dab01c0d2938a6a42fdfa213
Bogdan Pintican <ext-bogdan.pintican@aircall.io>
2025-04-04 10:26:03 +0300

    Add publish github action

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 npx webpack --config scripts/webpack/webpack.publish.js
+npx webpack --config scripts/webpack/webpack.publish.v2.js
 
 # deploy doc to gh-pages
 gh-pages -d demo_dist
 
-# deploy tests to gh-pages
-npm run coverage
-gh-pages -d coverage -e tests
